56f6be096b149d2db4e593165d883f68c4991c4d
DavilGM <127234685+DavilGM@users.noreply.github.com>
2024-08-03 20:15:22 +0700

    3/8

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,5 @@
 import db from '../models/index'
+import CRUDServices from '../services/CRUDServices';
 let getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll();
@@ -10,6 +11,37 @@ let getHomePage = async (req, res) => {
     }
 }
 
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs');
+}
+let postCRUD = async (req, res) => {
+    let massage = await CRUDServices.createNewUser(req.body);
+    console.log(massage)
+    return res.send('post crud');
+}
+let displayGetCRUD = async (req, res) => {
+    let data = await CRUDServices.getAllUser();
+    console.log(data)
+    return res.render('displayCRUD.ejs', {
+        dataTable: data
+    });
+}
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    if (userId) {
+        let userData = await CRUDServices.getUserInfoById(userId);
+        return res.send('ok');
+    }
+    else {
+        return res.send('Users not found!');
+    }
+
+
+}
 module.exports = {
-    getHomePage: getHomePage
+    getHomePage: getHomePage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
+    displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
 };
--- src/migrations/migration-create-specialtly.js
+++ src/migrations/migration-create-specialtly.js
@@ -14,6 +14,9 @@ module.exports = {
             image: {
                 type: Sequelize.INTEGER
             },
+            name: {
+                type: Sequelize.STRING
+            },
             createdAt: {
                 allowNull: false,
                 type: Sequelize.DATE
